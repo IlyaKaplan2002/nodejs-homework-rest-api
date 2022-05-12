e4571afd73050f046d745aa98c0d6ad578e51041
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-05-12 23:40:08 +0300

    ...

--- controllers/users/logIn.test.js
+++ controllers/users/logIn.test.js
@@ -1,19 +1,58 @@
+// const mongoose = require("mongoose");
+// const request = require("supertest");
+// require("dotenv").config();
+
 // const logIn = require("./logIn");
+// const app = require("../../app");
+// const { User } = require("../../service/schemas");
+// const userService = require("../../service/users");
 
-// // test("should returN response status 200 {status:'success', code:200, data: {token: String, user:{email: String, subscription: String}}}", () => {});
+// const { DB_HOST_TESTING } = process.env;
 
-// describe("login controller", () => {
-//   test("should return response status 200", async () => {
-//     const req = { body: { email: "test@mail.com", password: "123456" } };
-//     const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-//     const next = jest.fn();
+// const testedUser = { email: "test@mail.com", password: "123456" };
 
-//     await logIn(req, res, next);
-//     expect(res.status).toBe(200);
-//     expect(req.json).toBe({
-//       status: "success",
-//       code: 200,
-//       data: { token: String, user: { email: String, subscription: String } },
-//     });
+// // { email: "test@mail.com", password: "123456" } => status 200
+// // { email: "test@mail.com", password: "123456" } => should return {status:'success',code:200,data:{user:{email:String, subscription:String}}}
+// // { email: "test@mail.com", password: "123456" } => token should be
+// // { email: "test@mail.com", password: "123456" } => token should be equal with database token
+
+// // {} => status 400
+// // {} => {status:'failed', code:400, message:"\"password\" is required"}
+// // {"email":"test@mail.com"} => status 400
+// // {"email":"test@mail.com"} => {status:'failed', code:400, message:"\"password\" is required"}
+// // {"password": 123456} => status 400
+// // {"password": 123456} => {"status": "failed","code": 400,"message": "\"password\" must be a string"}
+// // {"password":"12345"} => status 400
+// // {"password":"12345"} => {"status": "failed","code": 400,"message": "\"password\" length must be at least 6 characters long"}
+// // {"password": "123456"} => status 400
+// // {"password": 123456} => {"status": "failed","code": 400,"message": "\"email\" is required"}
+// // {"email":"test","password": "123456"} => status 400
+// // {"password": 123456} => {"status": "failed","code": 400,"message": "\"email\" must be a valid email"}
+// // {"email":"test@mail.co","password": "123456"} => status 401
+// // {"email":"test@mail.co","password": "123456"} => {"status": "failed","code": 401,"message": "Email is wrong"}
+// // {"email":"test@mail.com","password": "1234567"} => status 401
+// // {"email":"test@mail.com","password": "1234567"} => {"status": "failed","code": 401,"message": "Password is wrong"}
+
+// describe("'/api/users/login' endpoint testing", () => {
+//   let server;
+
+//   beforeAll(() => {
+//     mongoose.connect(DB_HOST_TESTING).then(() => (server = app.listen(3000)));
+//   });
+
+//   afterAll((done) => {
+//     mongoose.disconnect(done);
+//     server.close();
+//   });
+
+//   beforeEach(() => {
+//     return request(app).post("/api/users/signup").send(testedUser);
+//   });
+//   afterEach((done) => {
+//     mongoose.connection.db.dropCollection("users").then(() => done());
+//   });
+
+//   test('{ email: "test@mail.com", password: "123456" } => status 200, {user:{email:String,subscription:String}}', async (done) => {
+//     return request(app).post("/api/users/login").send(testedUser).expect(200);
 //   });
 // });
